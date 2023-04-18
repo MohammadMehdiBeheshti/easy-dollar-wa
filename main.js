8c874e946ee22550c9bffebfee5bed48077b7ad0
Mohammad Mehdi Beheshti <shadymx@yahoo.com>
2023-04-18 23:38:15 +0430

    Still testing

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 "use strict";
 
-const users = [
-	[{ username: "jessica", password: "jessi123" }],
-	[{ username: "micheal", password: "mikethemachineweaver" }],
-	[{ username: "tyrone", password: "theroad123" }],
-];
+const users = [[{ username: "jessica", password: "jessi123" }]];
 
 const addUser = (username, password) => {
 	const newUser = [{ username, password }];
@@ -12,31 +8,23 @@ const addUser = (username, password) => {
 };
 
 const switchScreen = (screen) => {
-	// TODO: Make the main screen and put it in here
-
+	const dashboard = document.querySelector(".dashboard");
 	const loginFormCover = document.querySelector(".login-form-cover");
 	const registerFormCover = document.querySelector(".register-form-cover");
 
 	if (screen === 1) {
+		dashboard.style.display = "none";
 		registerFormCover.style.display = "none";
 		loginFormCover.style.display = "flex";
 	} else if (screen === 2) {
+		dashboard.style.display = "none";
 		loginFormCover.style.display = "none";
 		registerFormCover.style.display = "flex";
 	} else if (screen === 3) {
 		loginFormCover.style.display = "none";
 		registerFormCover.style.display = "none";
+		dashboard.style.display = "block";
 	}
 };
 
-document.querySelector(".logIn-form__submitbtn").addEventListener("click", (e) => {
-	e.preventDefault();
-	const loginFormUsername = document.querySelector(".logIn-form__usernameInput");
-	const loginFormPassword = document.querySelector(".logIn-form__passwordInput");
-
-	if (loginFormUsername.value && loginFormPassword.value) addUser(loginFormUsername.value, loginFormPassword.value);
-
-	console.log(users);
-});
-
-switchScreen(2);
+switchScreen(3);
